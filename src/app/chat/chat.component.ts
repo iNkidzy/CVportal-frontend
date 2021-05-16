6a5eb8620bc8ee1418a62b1ec468b639a76658c5
iNKidzy <nadq.nadia@gmail.com>
2021-05-16 18:31:24 +0200

    Subscription - Connesction

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,23 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {ChatService} from './shared/chat.service';
+import {Subject, Subscription} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, OnDestroy {
   message = new FormControl('');
   messages: string[] = [];
+  unsubscribe$ = new Subject(); // unsubscribes the subscription
   constructor(private chatService: ChatService) { }
 
   ngOnInit(): void {
     this.chatService.listenForMessages()
+      .pipe(
+        takeUntil(this.unsubscribe$)
+      )
       .subscribe(message => {
-      this.messages.push(message);
+        console.log(this.messages);
+        this.messages.push(message);
     });
-    console.log(this.messages);
+  }
+  ngOnDestroy(): void {
+    console.log('Destroyed');
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   sendMessage(): void {
