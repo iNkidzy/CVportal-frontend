815043d79e7c1901943b84047735f0a5f9645d0c
iNKidzy <nadq.nadia@gmail.com>
2021-05-16 15:57:02 +0200

    sendmsg +chatsLink + service

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { ChatRoutingModule } from './chat-routing.module';
 import { ChatComponent } from './chat.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { ChatComponent } from './chat.component';
   ],
   imports: [
     CommonModule,
-    ChatRoutingModule
+    ChatRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ChatModule { }
