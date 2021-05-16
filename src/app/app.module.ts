f90964afe009902190665a68eb5385e022d3d419
iNKidzy <nadq.nadia@gmail.com>
2021-05-16 15:14:47 +0200

    created chat comp with routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
-import { NgModule } from '@angular/core';
+import {Injectable, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
+import {Socket, SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
 
-const config: SocketIoConfig = {url: 'https://localhost:3000', options: {} };
+// const config: SocketIoConfig = {url: 'https://localhost:3000', options: {} };
+// const configChatBackend: SocketIoConfig = {url: 'https://localhost:3100', options: {} };
 
-
+@Injectable()
+export class SocketCV extends Socket {
+  constructor() {
+    super({url: 'https://localhost:3000', options: {} });
+  }
+}
+@Injectable()
+export class SocketChat extends Socket {
+  constructor() {
+    super({url: 'https://localhost:3100', options: {} });
+  }
+}
 
 @NgModule({
   declarations: [
@@ -17,9 +29,9 @@ const config: SocketIoConfig = {url: 'https://localhost:3000', options: {} };
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SocketIoModule.forRoot(config)
+    SocketIoModule
   ],
-  providers: [],
+  providers: [SocketCV, SocketChat],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
